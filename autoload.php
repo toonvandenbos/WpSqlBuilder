5314088729512730a84b4e2952fcc4c4d6aaa171
Toon Van den Bos <toon@whitecube.be>
2016-07-05 17:02:54 +0200

    Updated autoload files

--- autoload.php
+++ autoload.php
@@ -8,6 +8,8 @@ require_once( __DIR__ . '/core/Components/Operations/Select.php' );
 require_once( __DIR__ . '/core/Components/Table.php' );
 require_once( __DIR__ . '/core/Components/Column.php' );
 require_once( __DIR__ . '/core/Components/Joint.php' );
+require_once( __DIR__ . '/core/Components/Joints/Left.php' );
+require_once( __DIR__ . '/core/Components/Joints/Right.php' );
 require_once( __DIR__ . '/core/Components/Limit.php' );
 require_once( __DIR__ . '/core/Components/Conditions/Simple.php' );
 require_once( __DIR__ . '/core/Components/Conditions/Complex.php' );
